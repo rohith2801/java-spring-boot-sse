4e764686024e964a931d89de6cbe4645b8483fb8
azeem <azeem.baig@91signage.com>
2025-03-06 23:23:23 +0530

    removed embeded tomcat

--- src/main/java/com/demo/java_event/JavaEventApplication.java
+++ src/main/java/com/demo/java_event/JavaEventApplication.java
@@ -2,9 +2,15 @@ package com.demo.java_event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaEventApplication {
+public class JavaEventApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaEventApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEventApplication.class, args);
